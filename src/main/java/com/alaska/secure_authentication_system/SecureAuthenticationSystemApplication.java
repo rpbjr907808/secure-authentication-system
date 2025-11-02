b89209481edff5db09d881ca71705f57add348a3
rpbjr907808 <130121585+rpbjr907808@users.noreply.github.com>
2025-11-02 13:24:21 -0900

    Initial Spring Boot project setup with dependencies

---
+++ src/main/java/com/alaska/secure_authentication_system/SecureAuthenticationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.alaska.secure_authentication_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAuthenticationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAuthenticationSystemApplication.class, args);
+	}
+
+}
